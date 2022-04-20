1c876bba17a030a170022229191222b5c1a1c512
marcelomedrado <marcelo.medrado@sanar.com>
2022-04-20 14:32:45 -0400

    standalone json reader

---
+++ tree.py
@@ -0,0 +1,21 @@
+import final_project as fp
+import requests
+import json
+
+def read_json(filepath, encoding='utf-8'):
+    """Reads a JSON document, decodes the file content, and returns a list or dictionary if
+    provided with a valid filepath.
+
+    Parameters:
+        filepath (str): path to file
+        encoding (str): name of encoding used to decode the file
+
+    Returns:
+        dict/list: dict or list representations of the decoded JSON document
+    """
+
+    with open(filepath, 'r', encoding=encoding) as file_obj:
+        return json.load(file_obj)
+
+print(read_json('tree.json'))
+
